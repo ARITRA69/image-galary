1e9a776d371f5ad1e0fe5cd02fdd738574c4e01a
swarnadeep <aritrasarkar2002@gmail.com>
2023-10-21 10:26:13 +0530

    update

--- src/components/UnsplashImageFetcher.tsx
+++ src/components/UnsplashImageFetcher.tsx
@@ -182,7 +182,7 @@ const UnsplashImageFetcher: React.FC = () => {
                 </DialogContent>
               </Dialog>
             </div>
-          ),
+          )
         )}
       </div>
     </div>
